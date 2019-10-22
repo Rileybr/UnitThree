01f289789f8b94846b3fc88ddb503686f1f1e345
rileybr <brandon.riley@ssfs.org>
2019-10-22 13:31:25 -0400

    Checks if a number is odd, zero, positive, negative, non-positive, non-negative, a multiple of a second number and a factor of the second number

--- src/NumericComparisons.java
+++ src/NumericComparisons.java
@@ -19,11 +19,19 @@ public class NumericComparisons {
     public static boolean isNonPositive(int num) {
         return num <= 0;
     }
+    public static boolean isFactor(int num1, int num2) {
+        return num2 % num1 == 0;
+    }
+    public static boolean isMultiple(int num1, int num2) {
+        return num1 % num2 == 0;
+    }
 
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
-        System.out.println("Enter number: ");
+        System.out.println("Enter number 1: ");
         int num1 = scan.nextInt();
+        System.out.println("Enter number 2: ");
+        int num2 = scan.nextInt();
 
         System.out.println(num1 + " is odd: " + isOdd(num1));
         System.out.println(num1 + " is zero: " + isZero(num1));
@@ -31,5 +39,7 @@ public class NumericComparisons {
         System.out.println(num1 + " is negative: " + isNegative(num1));
         System.out.println(num1 + " is non-positive: " + isNonPositive(num1));
         System.out.println(num1 + " is non-negative: " + isNonNegative(num1));
+        System.out.println(num1 + " is is a factor of" + num2 + ": " + isFactor(num1, num2));
+        System.out.println(num1 + " is is a multiple of" + num2 + ": " + isMultiple(num1, num2));
     }
 }
